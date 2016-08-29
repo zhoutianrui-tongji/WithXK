4148dfa5d63febcc4e20ece25e48350e2ab2d824
iamjohnnyzhuang <johnny.zhuang93@gmail.com>
2016-08-29 21:17:48 +0800

    优化JS代码

--- js/count-time.js
+++ js/count-time.js
@@ -1,4 +1,3 @@
-var difference = undefined;
 /**
  * 计时器,每次重新计算效率有待优化
  * @param  {[type]} target [description]
@@ -8,47 +7,55 @@ var difference = undefined;
  * @param  {[type]} s      [description]
  * @return {[type]}        [description]
  */
-var countTime = function(target, d, h, m, s) {
-    var cur = new Date();
-    var his = new Date(target);
-    diff = calcDifference(cur.getTime() - his.getTime());
-    _countTime(d, h, m, s);
-    setInterval(function() {
-        _countTime(d, h, m, s);
-    }, 1000);
-}
+ const countTime = (target, d, h, m, s) => {
+ 	let cur = new Date().getTime();
+ 	let his = new Date(target).getTime();
+ 	diff = calcDifference(cur - his);
+ 	_countTime(d, h, m, s);
+ 	setInterval(function() {
+ 		_countTime(d, h, m, s);
+ 	}, 1000);
+ }
 
 
-function _countTime(d, h, m, s) {
-    $('#' + d).text(diff[0]);
-    $('#' + h).text(diff[1]);
-    $('#' + m).text(diff[2]);
-    $('#' + s).text(diff[3]);
-    diff[3] = (diff[3] + 1) % 60;
-    if (diff[3] == 0) {
-        diff[2] = (diff[2] + 1) % 60;
-        if (diff[2] == 0) {
-            diff[1] = (diff[1] + 1) % 24;
-            if (diff[1] == 0) {
-                diff[0] += 1;
-            }
-        }
-    }
-}
+/**
+ * 更改显示值
+ * @param  {[type]} d [description]
+ * @param  {[type]} h [description]
+ * @param  {[type]} m [description]
+ * @param  {[type]} s [description]
+ * @return {[type]}   [description]
+ */
+ const _countTime = (d, h, m, s) => {
+ 	$('#' + d).text(diff[0]);
+ 	$('#' + h).text(diff[1]);
+ 	$('#' + m).text(diff[2]);
+ 	$('#' + s).text(diff[3]);
+ 	diff[3] = (diff[3] + 1) % 60;
+ 	if (diff[3] == 0) {
+ 		diff[2] = (diff[2] + 1) % 60;
+ 		if (diff[2] == 0) {
+ 			diff[1] = (diff[1] + 1) % 24;
+ 			if (diff[1] == 0) {
+ 				diff[0] += 1;
+ 			}
+ 		}
+ 	}
+ }
+
 
 /**
  * 计算差距的具体时间
  * @param  {[type]} day [description]
  * @return {[type]}     [description]
  */
-function calcDifference(day) {
-    var days = Math.floor(day / (24 * 3600 * 1000));
-    var leave1 = day % (24 * 3600 * 1000);
-    var hours = Math.floor(leave1 / (3600 * 1000));
-    var leave2 = leave1 % (3600 * 1000);
-    var minutes = Math.floor(leave2 / (60 * 1000));
-    var leave3 = leave2 % (60 * 1000);
-    var seconds = Math.round(leave3 / 1000);
-    var res = new Array(days, hours, minutes, seconds);
-    return res;
-}
+ const calcDifference = day => {
+ 	const days = Math.floor(day / (24 * 3600 * 1000));
+ 	const leave1 = day % (24 * 3600 * 1000);
+ 	const hours = Math.floor(leave1 / (3600 * 1000));
+ 	const leave2 = leave1 % (3600 * 1000);
+ 	const minutes = Math.floor(leave2 / (60 * 1000));
+ 	const leave3 = leave2 % (60 * 1000);
+ 	const seconds = Math.round(leave3 / 1000);
+ 	return new Array(days, hours, minutes, seconds);
+ }
