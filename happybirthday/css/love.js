642a11ca1a599e187c1e45ad18ae6b8c3b7a1f28
Skychou <82795951+Skychou-source@users.noreply.github.com>
2022-01-21 18:49:41 +0800

    Add files via upload

--- happybirthday/css/love.js
+++ happybirthday/css/love.js
@@ -163,13 +163,13 @@
             ctx.scale(scale, scale);
             ctx.moveTo(0, 0);
     	    ctx.lineTo(15, 15);
-    	    ctx.lineTo(60, 15);
+    	    ctx.lineTo(70, 15);
             ctx.stroke();
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Come Baby", 23, 10);
+            ctx.font = "12px 微软雅黑,Verdana";
+            ctx.fillText("小可点一下", 23, 10);
             ctx.restore();
         },
         clear: function() {
